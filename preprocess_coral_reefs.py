a41fcc428a05db1d3cae6d8a3ac5d431127f73e3
juliakrtz <99036436+juliakrtz@users.noreply.github.com>
2022-02-05 18:57:51 +0000

    Create preprocess_coral_reefs.py

---
+++ preprocess_coral_reefs.py
@@ -0,0 +1,13 @@
+#import coral reef API
+import requests
+
+url = "https://geodata.hawaii.gov/arcgis/rest/services/CoastalMarine/MapServer/7/query?where=1%3D1&outFields=*&outSR=4326&f=jsonwhere=1%3D1&outFields=*&outSR=4326&f=json"
+
+payload={}
+headers = {
+  'Authorization': '5b3ce3597851110001cf624827861a30001c4ea7ac1a9dabbe858ce2'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
