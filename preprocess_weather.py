bee4cfbe7f20bb754059a64c6518f8676b2b7e53
juliakrtz <99036436+juliakrtz@users.noreply.github.com>
2022-02-06 14:35:44 +0000

    Create preprocess_weather.py

---
+++ preprocess_weather.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://api.stormglass.io/v2/weather/point"
+
+payload={}
+headers = {
+  'Authorization': '5b3ce3597851110001cf624827861a30001c4ea7ac1a9dabbe858ce2'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
