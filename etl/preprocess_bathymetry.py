5eb2149e95ec61f1cdd189983e2d273f02430d89
paulamegumi <paulamkawahara@gmail.com>
2022-02-07 15:03:58 +0000

    created schema file

--- etl/preprocess_bathymetry.py
+++ etl/preprocess_bathymetry.py
@@ -12,13 +12,13 @@ path_raster = r"data\original\bathymetry_hawaii.tif"
 #open raster as a gdal dataset (confirm it, not sure)
 input_raster = gdal.Open(path_raster)
 
-#path to 3 mile nautical boundary geojson
+#path to 3 mile nautical boundary 
 input_shape_3mile = "https://opendata.arcgis.com/datasets/82fb905c78064559905c8d932b63478b_31.geojson"
 
 #path to store output raster file with 3 mile clipped
 output_raster_3mile = "data\processed/output_3mile.tif"
 
-#coastline geojson of hawaii 
+#path to coastline  of hawaii 
 input_shape_coastline = "https://opendata.arcgis.com/datasets/045b1d5147634e2380566668e04094c6_3.geojson"
 
 #path to final bathymetry raster
@@ -31,4 +31,4 @@ ds = gdal.Warp(output_raster_3mile,
               cutlineDSName = input_shape_3mile,
               format = 'GTiff',
               dstNodata = -9999) # select the no data value
-ds=None #close the dataset. 
+ds=None #close the dataset
