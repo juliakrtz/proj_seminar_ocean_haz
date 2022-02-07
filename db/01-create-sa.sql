0135baf49de93d1144db05b084ef42e03976e96a
Julia Kraatz <julia.kraatz@gmail.com>
2022-02-07 17:43:25 +0000

    updated sa schema

--- db/01-create-sa.sql
+++ db/01-create-sa.sql
@@ -15,28 +15,33 @@ CREATE TABLE bathymetry (
 CREATE TABLE bottom_type (
     id serial PRIMARY KEY, 
     code integer, 
-    seabed "string" /*change this? */ 
+    seabed varchar
 ); 
 
 CREATE TABLE coral_reefs (
     id serial  PRIMARY KEY,
     acres integer,
-    severity "string",
+    severity varchar,
     st_area integer, 
     st_perimeter integer
 ); 
 
 CREATE TABLE shark_attacks (
     id serial PRIMARY KEY, 
-    date_time timestamp, 
-    location_attack "string",
-    activity "string",
+    date_attack timestamp, 
+    time_attack timestamp,
+    island varchar,
+    location_attack varchar,
+    location_attack_2 varchar,
+    dist_from_shore varchar, 
+    activity varchar,
     water_clarity integer, 
-    shark "string"
+    ocean_depth integer,
+    shark_type varchar
 ); 
 
 CREATE TABLE hazardous_areas (
     id serial PRIMARY KEY,
     pixel_depth integer, 
-    hazardous_y_n "string"
+    hazardous_y_n varchar
 );
