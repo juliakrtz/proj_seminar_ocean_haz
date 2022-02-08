885038a1654a953af94c65dd228072b8a3cb9283
Julia Kraatz <julia.kraatz@gmail.com>
2022-02-08 18:21:05 +0000

    updated shark attacks in python

--- preprocess_sharkattack.py
+++ preprocess_sharkattack.py
@@ -1,16 +1,9 @@
-#preprocess shark attacks data
-
+#import
 import pandas as pd
-import geocoder
-from sqlalchemy import create_engine 
-
-DB_SCHEMA = "sa"
-TABLE = "shark_attacks"
-DOWNLOAD_DIR = "data/original"
-PROCESSED_DIR = "data/processed"
-
+import geopandas
+import geocoder 
+from sqlalchemy import create_engine
 shark_attacks = "data\original\shark_attacks.csv"
-
 df = pd.read_csv(shark_attacks, encoding = 'cp1252', sep=",", header="infer")
 
 #geocode shark_attacks dataframe 
@@ -18,12 +11,18 @@ def geocoding(input_island):
     g = geocoder.osm(input_island)
     return g.osm['x'], g.osm['y']
 
+##apply function to dataframe in island column 
 df['locations'] = df['island'].apply(geocoding)
 df[['lon','lat']] = pd.DataFrame(df['locations'].tolist(), 
         index = df.index)
 
 df.head()
 
+##upload geocoded dataframe to postgis
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/geotech_ocean_haz")
-gdf.to_postgis("shark_attacks", engine)
+#set up database connection engine
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/geotech_ocean_haz')
+df.to_sql(
+    con=engine,
+    name= "shark_attacks"
+)
