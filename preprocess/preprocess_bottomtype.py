1d7d1fc8c1fca243ee345284fc5d10bf5c1ef176
Julia Kraatz <julia.kraatz@gmail.com>
2022-02-10 14:40:53 +0000

    updated bottom type

--- preprocess/preprocess_bottomtype.py
+++ preprocess/preprocess_bottomtype.py
@@ -1,30 +1,27 @@
 #pre processing bottom type shapefile and loading it to db
 import geopandas
+import pandas 
 from sqlalchemy import create_engine
 
 #import requests
 
 DB_SCHEMA = "sa"
 TABLE = "bottom_type"
-DOWNLOAD_DIR = "data/original"
-PROCESSED_DIR = "data/processed"
+
 
 url_bottom_type = "https://opendata.arcgis.com/datasets/4780150a51064c37bc627e2dcc4df615_2.geojson"
 
-gdf_bottom_type = geopandas.read_file(url_bottom_type)
+gdf = geopandas.read_file(url_bottom_type)
 
-
-payload={}
-headers = {
-  'Authorization': '5b3ce3597851110001cf624827861a30001c4ea7ac1a9dabbe858ce2'
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+# payload={}
+# headers = {
+#   'Authorization': '5b3ce3597851110001cf624827861a30001c4ea7ac1a9dabbe858ce2'
+# }
+# response = requests.request("GET", url, headers=headers, data=payload)
+# print(response.text)
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/geotech_ocean_haz')
-df.to_sql(
+gdf.to_postgis(
     con=engine,
-    name= "bottom_type"
+    name=TABLE,
 )
