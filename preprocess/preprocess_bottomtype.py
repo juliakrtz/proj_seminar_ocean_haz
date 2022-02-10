0f6703c6df711ef290d3770c63fd02e674d9063d
Julia Kraatz <julia.kraatz@gmail.com>
2022-02-10 14:23:07 +0000

    updated changes

--- preprocess/preprocess_bottomtype.py
+++ preprocess/preprocess_bottomtype.py
@@ -1,5 +1,6 @@
 #importing bottom type API
 import geopandas
+from sqlalchemy import create_engine
 
 #import requests
 
@@ -13,8 +14,6 @@ url_bottom_type = "https://opendata.arcgis.com/datasets/4780150a51064c37bc627e2d
 gdf_bottom_type = geopandas.read_file(url_bottom_type)
 
 
-
-
 payload={}
 headers = {
   'Authorization': '5b3ce3597851110001cf624827861a30001c4ea7ac1a9dabbe858ce2'
@@ -23,3 +22,9 @@ headers = {
 response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
+
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/geotech_ocean_haz')
+df.to_sql(
+    con=engine,
+    name= "bottom_type"
+)
