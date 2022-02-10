72d109e8ad50760412fd00ad590c6e3094c3bfc0
paulamegumi <paulamkawahara@gmail.com>
2022-02-10 21:10:00 +0000

    edited bathymtry data

--- preprocess_bathymetry.py
+++ preprocess_bathymetry.py
@@ -1,10 +1,12 @@
 #preprocess bathymetry data
 #1. Clip raster from the 3 mile nautical boundary 
-#2. Burn coastline boundary from raster 
 #3. Load final raster into the database
 
 #import standard modules
+from click import password_option
 from osgeo import gdal, ogr
+import psycopg2
+import os
 
 #path to original bathymetry raster filer
 path_raster = r"data\original\bathymetry_hawaii.tif"
@@ -32,3 +34,18 @@ ds = gdal.Warp(output_raster_3mile,
               format = 'GTiff',
               dstNodata = -9999) # select the no data value
 ds=None #close the dataset
+
+
+conn = psycopg2.connect(
+    host = 'localhost',
+    port = 5432,
+    user = 'postgres',
+    dbname = 'geotech_ocean_haz',
+    password = 'postgres'
+    )
+curs = conn.cursor()
+curs.execute("SET postgis.gdal_enabled_drivers = 'ENABLE_ALL';")
+os.system('raster2pgsql "%s" > temp.sql'%output_raster_3mile)
+curs.execute(open('temp.sql','r').read())
+
+
