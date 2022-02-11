4ef77440673306f5c79ab8891863980e6d16333e
paulamegumi <paulamkawahara@gmail.com>
2022-02-11 18:03:25 +0000

    created jupyter notebook3

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 #create Flask application
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return "Hello world"
+# @app.route('/')
+# def welcome():
+#     return "Hello world"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=105)
 
+#set URI to the database to use
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/geotech_ocean_haz'
 
+#use the application object as a parameter to create an object of class SQLAlchemy
 db = SQLAlchemy(app)
 
+#declare shark_attack object model
 class sharkattacks(db.Model):
     __tablename__ = "shark_attacks"
     #create the columns
@@ -30,10 +33,8 @@ class sharkattacks(db.Model):
     water_clarity = db.Column(db.Integer)
     ocean_depth = db.Column(db.Integer)
     shark_type = db.Column(db.Integer)
-); 
-    id = db.Column(db.Integer, primary_key=True)
-    pickup_datetime = db.Column(db.DateTime())
-    dropoff_datetime = db.Column(db.DateTime())
-    passenger_count = db.Column(db.Integer)
-    geojson = db.Column(db.Text)
 
+
+
+    def __init__(self, date_attack, time_attack, island, location_attack, location_attack_2, dist_from_shore, activity, water_clarity, ocean_depth, shark_type):
+        self.date_attack = date_attack
