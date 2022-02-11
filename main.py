680c8dddf10a4493a31ff183909ea55aaac3fc5e
paulamegumi <paulamkawahara@gmail.com>
2022-02-11 12:40:29 +0000

    edited main

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ db = SQLAlchemy(app)
 class sharkattacks(db.Model):
     __tablename__ = "shark_attacks"
     #create the columns
-    id = serial PRIMARY KEY, 
-    date_attack timestamp, 
-    time_attack timestamp,
-    island varchar,
-    location_attack varchar,
-    location_attack_2 varchar,
-    dist_from_shore varchar, 
-    activity varchar,
-    water_clarity integer, 
-    ocean_depth integer,
-    shark_type varchar
+    id = db.Column(db.Integer, primary_key= True) 
+    date_attack = db.Column(db.DateTime()) 
+    time_attack = db.Column(db.DateTime()) 
+    island = db.Column(db.Integer)
+    location_attack = db.Column(db.Integer)
+    location_attack_2 = db.Column(db.Integer)
+    dist_from_shore = db.Column(db.Integer)
+    activity = db.Column(db.Integer)
+    water_clarity = db.Column(db.Integer)
+    ocean_depth = db.Column(db.Integer)
+    shark_type = db.Column(db.Integer)
 ); 
     id = db.Column(db.Integer, primary_key=True)
     pickup_datetime = db.Column(db.DateTime())
