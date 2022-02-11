4efbd197f866f004182aa6659cbcaa98aa86d248
Julia Kraatz <julia.kraatz@gmail.com>
2022-02-11 18:00:43 +0000

    flask set-up

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
-from imp import is_frozen_package
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from  shapely.geometry import Point as Shapely_pt, mapping
+from geojson import Point as Geoj_pt, Polygon as Geoj_polygon, Feature, Featurecollection
+
 
 #create Flask application
 app = Flask(__name__)
-
-@app.route('/')
-def welcome():
-    return "Hello world"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/geotech_ocean_haz'
 
 db = SQLAlchemy(app)
 
+#create the columns, matches table from postgis
 class sharkattacks(db.Model):
     __tablename__ = "shark_attacks"
-    #create the columns
-    id = serial PRIMARY KEY, 
-    date_attack timestamp, 
-    time_attack timestamp,
-    island varchar,
-    location_attack varchar,
-    location_attack_2 varchar,
-    dist_from_shore varchar, 
-    activity varchar,
-    water_clarity integer, 
-    ocean_depth integer,
-    shark_type varchar
+    __table_args__ = {"schema": "public"}
+    id = db.Columb(db.Integer, primary_key = True)
+    date_attack = db.Column(db.timestamp()), 
+    time_attack = db.Column(db.timestamp()),
+    island = db.Column(db.varchar()),
+    location_attack = db.Column(db.varchar()),
+    location_attack_2 = db.Column(db.varchar()),
+    dist_from_shore = db.Column(db.varchar()),
+    activity = db.Column(db.varchar()),
+    water_clarity = db.Column(db.integer()),
+    ocean_depth = db.Column(db.integer()), 
+    shark_type = db.Column(db.varchar()),
 ); 
-    id = db.Column(db.Integer, primary_key=True)
-    pickup_datetime = db.Column(db.DateTime())
-    dropoff_datetime = db.Column(db.DateTime())
-    passenger_count = db.Column(db.Integer)
-    geojson = db.Column(db.Text)
+
+@app.route('/')
+def index():
+    return render_template('') #put html here 
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=TRUE) 
+
+#to create a class
+    # id = db.Column(db.Integer, primary_key=True)
+    # pickup_datetime = db.Column(db.DateTime())
+    # dropoff_datetime = db.Column(db.DateTime())
+    # passenger_count = db.Column(db.Integer)
+    # geojson = db.Column(db.Text)
 
