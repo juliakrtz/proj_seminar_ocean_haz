04004534f692c756b41bc5154c247e526852ce08
Julia Kraatz <julia.kraatz@gmail.com>
2022-02-22 14:57:57 +0000

    deleted unnecessary files

--- db/00-create-schemas.sql
+++
@@ -1,4 +0,0 @@
-CREATE EXTENSION postgis;
-
-CREATE SCHEMA sa; -- stagging area
-CREATE SCHEMA pa; -- production area
--- etl/__init__.py
+++
@@ -1,6 +0,0 @@
-from .logs import die, info, done, init_logger
-#from .ds import write_csv, read_csv, download_data
-from .db import DBController, host
-from .config import read_config
-
-init_logger()
